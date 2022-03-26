029192c58bd6bc7d970de9270b9e0416d3bb91a0
Eugen695029 <pavlov_eugeny@mail.ru>
2022-03-26 22:45:21 +0300

    v3

--- my-app/src/socket/index.js
+++ my-app/src/socket/index.js
@@ -4,7 +4,7 @@ const options = {
     "force new connection": true,
     reconnectionAttemps: "Infinity",
     timeout: 10000,
-    transports: ["wedsocket"]
+    transports: ["websocket"]
 }
 
 const socket = io('http://localhost:3001',options);
