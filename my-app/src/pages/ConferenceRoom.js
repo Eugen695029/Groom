df3e7af884f535b5cfff5bba8e795cc019b7f114
Eugen695029 <pavlov_eugeny@mail.ru>
2022-03-26 20:15:01 +0300

    v

--- my-app/src/pages/ConferenceRoom.js
+++ my-app/src/pages/ConferenceRoom.js
@@ -12,9 +12,39 @@ import phonePNG from '../img/phone.png'
 import messengerPNG from '../img/messenger.png'
 import LOCAL_VIDEO from '../hooks/useWebRTC.js'
 
+function layout(clientsNumber = 1) {
+    const pairs = Array.from({length: clientsNumber})
+      .reduce((acc, next, index, arr) => {
+        if (index % 2 === 0) {
+          acc.push(arr.slice(index, index + 2));
+        }
+  
+        return acc;
+      }, []);
+  
+    const rowsNumber = pairs.length;
+    const height = `${100 / rowsNumber}%`;
+  
+    return pairs.map((row, index, arr) => {
+  
+      if (index === arr.length - 1 && row.length === 1) {
+        return [{
+          width: '100%',
+          height,
+        }];
+      }
+  
+      return row.map(() => ({
+        width: '50%',
+        height,
+      }));
+    }).flat();
+  }
+
 function ConferenceRoom() {
     const {id: roomID} = useParams();
     const {clients, provideMediaRef} = useWebRTC(roomID);
+    const videoLayout = layout(clients.length);
 
     const [visibility, v] = useState(false);
 
@@ -30,19 +60,21 @@ function ConferenceRoom() {
             <div className={classes.containerMain}>
                 <div className={classes.videContainer}>
                     <div className={classes.containerItem1}>
-                        {clients.map((clientID) => {
-                            return(
-                                <div>
-                                    <video
-                                        ref={ instance => {
-                                         provideMediaRef(clientID, instance)
-                                        }}
-                                        autoPlay
-                                        playsInline
-                                        muted={clientID === LOCAL_VIDEO}
-                                    />
-                                </div>
-                            )
+                        {clients.map((clientID, index) => {
+                        return (
+                            <div key={clientID} style={videoLayout[index]} id={clientID}>
+                            <video
+                              width='auto'
+                              height='auto'
+                              ref={instance => {
+                                provideMediaRef(clientID, instance);
+                              }}
+                              autoPlay
+                              playsInline
+                              muted={clientID === LOCAL_VIDEO}
+                              />
+                            </div>
+                        );
                         })}
                     </div>
                     <div className={classes.containerItem2}>
